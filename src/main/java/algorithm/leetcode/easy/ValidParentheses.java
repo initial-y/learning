2d83d69d72d8cefc4c0188638694e2a2acdb23b0
yang <initial_yang@163.com>
2020-04-10 23:07:59 +0800

    add ValidParentheses

---
+++ src/main/java/algorithm/leetcode/easy/ValidParentheses.java
@@ -0,0 +1,69 @@
+package algorithm.leetcode.easy;
+
+import java.util.Stack;
+
+/**
+ * @ClassName ValidParentheses
+ * @Descripiton
+ * @Author initial_yang
+ * @Date 2020/4/10
+ */
+public class ValidParentheses {
+
+    public boolean isValid(String s) {
+        if ("".equals(s)) {
+            return true;
+        }
+
+        Stack<Character> stack = new Stack();
+        char[] charArr = s.toCharArray();
+        for (int i = 0; i < charArr.length; i++) {
+            if (!stack.isEmpty()) {
+                if (charArr[i] == ')' && stack.peek() == '(') {
+                    stack.pop();
+                    continue;
+                }
+                if (charArr[i] == ']' && stack.peek() == '[') {
+                    stack.pop();
+                    continue;
+                }
+                if (charArr[i] == '}' && stack.peek() == '{') {
+                    stack.pop();
+                    continue;
+                }
+                stack.push(charArr[i]);
+            } else {
+                stack.push(charArr[i]);
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public boolean isValidTwo(String s) {
+        if ("".equals(s)) {
+            return true;
+        }
+
+        Stack stack = new Stack();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            // 左括号
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else { // 右括号
+                // 当前c是右括号 但是stack是空
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char topChar = (char) stack.peek();
+                if (topChar == '(' && c == ')' || topChar == '[' && c == ']' || topChar == '{' && c == '}') {
+                    stack.pop();
+                } else {
+                    stack.push(c);
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+}
--- src/test/java/algorithm/leetcode/easy/EasyTest.java
+++ src/test/java/algorithm/leetcode/easy/EasyTest.java
@@ -1,9 +1,39 @@
 package algorithm.leetcode.easy;
 
+import org.junit.Test;
+
+import java.util.Stack;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 /**
  * @className: EasyTest
  * @author: yangxin
  * @date: 2020/4/9
  */
 public class EasyTest {
+
+    @Test
+    public void test_ValidParentheses_isValid() {
+        assertTrue(new ValidParentheses().isValid("()"));
+        assertTrue(new ValidParentheses().isValid("()[]{}"));
+        assertFalse(new ValidParentheses().isValid("(]"));
+        assertFalse(new ValidParentheses().isValid("([)]"));
+    }
+
+    @Test
+    public void test_ValidParentheses_isValidTwo() {
+        assertTrue(new ValidParentheses().isValidTwo("()"));
+        assertTrue(new ValidParentheses().isValidTwo("()[]{}"));
+        assertFalse(new ValidParentheses().isValidTwo("(]"));
+        assertFalse(new ValidParentheses().isValidTwo("([)]"));
+    }
+
+    @Test
+    public void test_stack() {
+        Stack stack = new Stack();
+        System.out.println(stack.peek());
+    }
+
 }
