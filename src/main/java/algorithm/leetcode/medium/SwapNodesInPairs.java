60b34c4d63294a346d48bb2be08da1eba5bc3c48
yang <initial_yang@163.com>
2021-06-17 23:56:54 +0800

    24. swap nodes in pairs

--- src/main/java/algorithm/leetcode/medium/SwapNodesInPairs.java
+++ src/main/java/algorithm/leetcode/medium/SwapNodesInPairs.java
@@ -11,8 +11,43 @@ import algorithm.leetcode.ListNode;
  */
 public class SwapNodesInPairs {
 
+    /**
+     * 递归
+     * @param head
+     * @return
+     */
     public ListNode swapPairs(ListNode head) {
+        // 终止条件
+        if (head == null || head.next == null) {
+            return head;
+        }
 
-        return null;
+        // 本级递归
+        ListNode next = head.next;
+        head.next = swapPairs(next.next);
+        next.next = head;
+        // 返回值
+        return next;
+    }
+
+    /**
+     * 迭代
+     */
+    public ListNode swapPairsIterator(ListNode head) {
+        ListNode dummy = new ListNode(-1, head);
+        ListNode tmp = dummy;
+        while (tmp.next != null && tmp.next.next != null) {
+            // 两两交换, 注意使用临时变量暂存值
+            ListNode next1 = tmp.next;
+            ListNode next2 = tmp.next.next;
+            tmp.next = next2;
+            next1.next = next2.next;
+            tmp.next.next = next1;
+
+            // 刷新tmp
+            tmp = tmp.next.next;
+        }
+
+        return dummy.next;
     }
 }
