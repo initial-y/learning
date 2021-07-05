c80cf07012b1fedc1ddbcec30fa7dbf6b0f82b6f
xin.yang2 <xin.yang2@dmall.com>
2021-07-05 18:55:23 +0800

    int 232

---
+++ src/main/java/algorithm/leetcode/easy/ImplementQueueUsingStacks.java
@@ -0,0 +1,48 @@
+package algorithm.leetcode.easy;
+
+import java.util.Stack;
+
+/**
+ * @className ImplementQueueUsingStacks
+ * @description 
+ * @author xin.yang
+ * @date 2021/07/05
+ * @num 232
+ * @link https://leetcode-cn.com/problems/implement-queue-using-stacks
+ */
+public class ImplementQueueUsingStacks {
+
+    private Stack stack;
+
+    private int size;
+
+
+    /** Initialize your data structure here. */
+    public ImplementQueueUsingStacks() {
+
+    }
+
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+
+    }
+
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+
+        return 0;
+    }
+
+    /** Get the front element. */
+    public int peek() {
+
+        return 0;
+    }
+
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+
+        return false;
+    }
+
+}
