80042140cca8291db45e3ca7064dcada178899c9
xin.yang2 <xin.yang2@dmall.com>
2021-11-18 20:03:01 +0800

    lc 105

---
+++ src/main/java/algorithm/leetcode/medium/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
@@ -0,0 +1,48 @@
+package algorithm.leetcode.medium;
+
+import algorithm.leetcode.TreeNode;
+
+/**
+ * @author initial.y
+ * @className ConstructBinaryTreeFromPreorderAndInorderTraversal
+ * @description
+ * @date 2021/11/18
+ * @num 105
+ */
+public class ConstructBinaryTreeFromPreorderAndInorderTraversal {
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        return this.build(preorder, 0, preorder.length - 1,
+                inorder, 0, inorder.length - 1);
+    }
+
+    public TreeNode build(int[] preorder, int preLow, int preHigh,
+                          int[] inorder, int inLow, int inHigh) {
+        if (preLow > preHigh || inLow > inHigh) {
+            return null;
+        }
+        // 根据根节点将中序一分为2
+        int rootIndex = 0;
+        int rootValue = preorder[preLow];
+        for (int i = inLow; i < inHigh; i++) {
+            if (rootValue == inorder[i]) {
+                rootIndex = i;
+            }
+        }
+
+        /**
+         * 确定下标过程
+         * 在中序遍历中, root左侧节点的数据范围inorder[inLow, rootIndex - 1], 节点个数 = rootIndex - inLow
+         *            root右侧节点的数据范围inorder[rootIndex + 1, inHigh]
+         * 在前序遍历中, 第一个节点为根节点, 根节点左子节点的范围preorder[preLow + 1, preLow + leftSize]
+         *                             根节点右子节点的范围preorder[preLow + leftSize +1, preHigh]
+         */
+        int leftSize = rootIndex - inLow;
+        // 前序确定根节点
+        TreeNode root = new TreeNode(preorder[preLow]);
+        // 递归查找左右节点
+        root.left = this.build(preorder, preLow + 1 , preLow + leftSize, inorder, inLow, rootIndex - 1);
+        root.right = this.build(preorder, preLow + leftSize + 1 , preHigh, inorder, rootIndex + 1, inHigh);
+        return root;
+    }
+}
--- src/test/java/base/java/Tests.java
+++ src/test/java/base/java/Tests.java
@@ -1,11 +1,15 @@
 package base.java;
 
 
+import org.apache.commons.collections4.CollectionUtils;
 import org.junit.Test;
 
+import java.math.BigDecimal;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * @className Test
@@ -26,4 +30,34 @@ public class Tests {
         LocalDateTime time = LocalDateTime.of(2021, 9, 10, 10,44, 55, 435);
         System.out.println(time.plusSeconds(47704 + 1));
     }
+
+
+    @Test
+    public void testCollectionSubtract() {
+        List<Integer> cxList = Arrays.asList(1,2,3,4);
+        List<Integer> chList = new ArrayList<>();
+        List<Integer> diff = (List<Integer>) CollectionUtils.union(cxList, chList);
+        System.out.println(CollectionUtils.subtract(cxList, chList));
+    }
+
+    @Test
+    public void testSet() {
+        Set<BigDecimal> set = new HashSet<>();
+        set.add(new BigDecimal("0.130"));
+        set.add(new BigDecimal("0.1300"));
+        set = set.stream().map(BigDecimal :: stripTrailingZeros).collect(Collectors.toSet());
+        System.out.println(set);
+    }
+
+
+    @Test
+    public void testBigDecimal() {
+        List<BigDecimal> c1 = Arrays.asList(new BigDecimal("0.13"), new BigDecimal("0.1100"));
+        List<BigDecimal> c2 = Arrays.asList(new BigDecimal("0.09"), null);
+        List<BigDecimal> union = (List<BigDecimal>) CollectionUtils.union(c1, c2);
+//        List<BigDecimal> union = CollectionUtils.union(c1, c2).stream().map(BigDecimal::stripTrailingZeros).collect(Collectors.toList());
+        System.out.println(union);
+    }
+
+
 }
